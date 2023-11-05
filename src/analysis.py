8b7602116757e31809cc0702b859fe9d1d51cc73
Seventyy <242227@edu.p.lodz.pl>
2023-11-05 02:55:31 +0100

    error calculating methods implementation

--- src/analysis.py
+++ src/analysis.py
@@ -1,16 +1,64 @@
 import numpy as np
+import math
 
-def mse(old_img, new_img):
-    pass
+def mse(org_img, img):
+    val = 0
+    
+    for y in range(org_img.shape[1]):
+        for x in range(org_img.shape[0]):
+            val += pow(org_img[x, y] - img[x, y], 2)
+    val = val / (org_img.shape[0] * org_img.shape[1])
+    
+    return val
 
-def pmse(old_img, new_img):
-    pass
+def pmse(org_img, img):
+    val = 0
+    img_max = 0
+    
+    for y in range(org_img.shape[1]):
+        for x in range(org_img.shape[0]):
+            val += pow(org_img[x, y] - img[x, y], 2)
+            img_max = max(img_max, org_img[x,y])
+    val = val / (org_img.shape[0] * org_img.shape[1]) / pow(img_max, 2)
+    
+    return val
 
-def snr(old_img, new_img):
-    pass
+def snr(org_img, img):
+    val = 0
+    val_1 = 0
+    val_2 = 0
+    
+    for y in range(org_img.shape[1]):
+        for x in range(org_img.shape[0]):
+            val_1 += pow(org_img[x, y])
+            val_2 += pow(org_img[x, y] - img[x, y], 2)
+    val = 10 * math.log(val_1 / val_2, 10)
 
-def psnr(old_img, new_img):
-    pass
+    return val
 
-def md(old_img, new_img):
-    pass
+def psnr(org_img, img):
+    val = 0
+    val_1 = 0
+    val_2 = 0
+    img_max = 0
+    
+    for y in range(org_img.shape[1]):
+        for x in range(org_img.shape[0]):
+            img_max = max(img_max, org_img[x,y])
+    
+    for y in range(org_img.shape[1]):
+        for x in range(org_img.shape[0]):
+            val_1 += pow(img_max, 2)
+            val_2 += pow(org_img[x, y] - img[x, y], 2)
+    val = 10 * math.log(val_1 / val_2, 10)
+
+    return val
+    
+def md(org_img, img):
+    val = 0
+    
+    for y in range(org_img.shape[1]):
+        for x in range(org_img.shape[0]):
+            val = max(val, abs(org_img[x,y] - img[x,y]))
+
+    return val
