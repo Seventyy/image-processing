5f146f8dfc4311296b1f5e88c65ca2ab8a40bb66
Mateusz Kubiak <matkubiak888@gmail.com>
2023-11-05 01:57:01 +0100

    feat: lookup for elementary ops

--- src/elementary.py
+++ src/elementary.py
@@ -1,18 +1,42 @@
+import numpy as np
+
 def brightness(img, value: int):
+    lookup = []
+    for n in range(256):
+        new_val = n + value
+        if (new_val < 0):
+            new_val = 0
+        if (new_val > 255):
+            new_val = 255
+        lookup.append(new_val)
+
     for x in range(img.shape[0]):
         for y in range(img.shape[1]):
-            img[x, y] = min(max(img[x, y] + value, 0), 255)
+            img[x, y] = lookup[img[x,y]]
     return img
 
 def negative(img):
+    lookup = []
+    for n in range(256):
+        new_val = 255 - n
+        lookup.append(new_val)
+
     for x in range(img.shape[0]):
         for y in range(img.shape[1]):
-            img[x, y] = 255 - img[x, y]
+            img[x, y] = lookup[img[x, y]]
     return img
 
 def contrast(img, value: float):
+    lookup = []
+    for n in range(256):
+        new_val = value * (n - 125) + 125
+        if (new_val < 0):
+            new_val = 0
+        if (new_val > 255):
+            new_val = 255
+        lookup.append(new_val)
+
     for x in range(img.shape[0]):
         for y in range(img.shape[1]):
-            raw = value * (img[x, y] - 125) + 125
-            img[x, y] = max(min(raw, 255), 0)
+            img[x, y] = lookup[img[x, y]]
     return img
