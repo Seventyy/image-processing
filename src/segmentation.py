2671923737b325138a7ce0b2b46673e16f3db6ea
Mateusz Kubiak <matkubiak888@gmail.com>
2024-01-09 22:19:18 +0100

    los segmentos muy divertidos

--- src/segmentation.py
+++ src/segmentation.py
@@ -36,66 +36,85 @@ class subframe:
         if True in img[x1:x2+1, y2]:
             self.y2 = min(self.y2 + 1, self.max_y)
 
-def detect_region(img):
-    old_region = np.full(img.shape, False, dtype=bool)
-    new_region = old_region.copy()
-    se = get_sample_se('iii')
+def grow(mask, img, error):
+    four_adj = [
+        [-1, 0],
+        [1, 0],
+        [0, -1],
+        [0, 1]
+    ]
+    [width, height] = img.shape
+    changed = 0 # no of pixels changed
+    
+    for x in range(1, width-1):
+        for y in range(1, height-1):
+            if mask[x,y] == False:
+                continue
+            
+            # compare pixels
+            comp = img[x,y] # white pixel
 
+            for adj in four_adj:
+                new_val = img[x + adj[0], y + adj[1]]
+                
+                if mask[x + adj[0], y + adj[1]] == True:
+                    continue
+                
+                if abs(int(new_val) - int(comp)) < error:
+                    mask[x + adj[0], y + adj[1]] = True
+                    changed += 1
+    return [mask, changed]
+
+def detect_region(region, img, error, seedx, seedy):
     # choose a seed
-    [seedx, seedy] = [0, 0]
-    while(True):
-        seedx = random.randint(0, img.shape[0])
-        seedy = random.randint(0, img.shape[1])
-        if img[seedx, seedy]:
-            break
-    old_region[seedx, seedy] = 1
+    # [seedx, seedy] = [0, 0]
+    # while(True):
+    #     seedx = random.randint(0, img.shape[0])
+    #     seedy = random.randint(0, img.shape[1])
+    #     if img[seedx, seedy]:
+    #         break
+    region[seedx, seedy] = 1
 
-    frame = subframe(seedx, seedy, old_region.shape[0], old_region.shape[1])
+    frame = subframe(seedx, seedy, region.shape[0], region.shape[1])
+    steps = 0
 
-    iters = 0
     # grow
     while (True):
-        iters += 1
-        print(iters)
+        steps += 1
+        print(f'step: {steps}')
         
-        # dilation of pixels in working frame
-        new = dilation(frame.get(old_region).copy(), se)
-        frame.set(new_region, new)
+        # growth of region in working frame
+        [new, changed] = grow(frame.get(region).copy(), frame.get(img), error)
+        frame.set(region, new)
 
-        # intersection with orginal img
-        for x in range(new_region.shape[0]):
-            for y in range(new_region.shape[1]):
-                new_region[x, y] = new_region[x, y] and img[x, y]
-
-        if np.array_equal(new_region, old_region):
+        if changed == 0:
             break
         else:
-            old_region = new_region.copy()
-            frame.try_expand(new_region)
-    
-    return old_region
+            frame.try_expand(region)
+    print([seedx, seedy])
+    return region
 
 def region_growing(img):
     [width, height] = img.shape
-
-    binary = np.full(img.shape, 0, dtype=bool)
-
-    treshold = 170
-
-    for x in range(width):
-        for y in range(height):
-            binary[x,y] = img[x,y] > treshold
-
-    region = detect_region(binary)
-    for x in range(width):
-        for y in range(height):
-            if region[x,y]:
-                img[x,y] = 255
-            elif binary[x,y]:
-                img[x,y] = 128
-            else:
-                img[x,y] = 0
-            # if not region[x,y]:
-            #     img[x,y] = max(min(img[x,y] - 100, 155), 0)
     
+    error = 5
+    seeds = [
+        [184, 168],
+        [68, 492]
+    ]
+
+    region = np.full(img.shape, False, dtype=bool)
+
+    for s in seeds:
+        new_region = np.full(img.shape, False, dtype=bool)
+        new_region = detect_region(new_region, img, error, s[0], s[1])
+
+        for x in range(width):
+            for y in range(height):
+                region[x,y] = new_region[x,y] or region[x,y]
+
+    for x in range(width):
+        for y in range(height):
+            if region[x,y] == True:
+                img[x,y] = 0
     return img
